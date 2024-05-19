b1d4aee6cf6c33c86f6aaf56d1351939771207ac
Okan Karasoglu <okan_karasoglu@hotmail.com>
2024-05-19 15:09:42 +0300

    Press E to travel text added

--- Assets/Scripts/PortalTravel.cs
+++ Assets/Scripts/PortalTravel.cs
@@ -2,18 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Unity.UI;
+using TMPro;
 
 
 public class PortalTravel : MonoBehaviour
 {
     public string sceneNameToLoad; // The name of the scene to load on collision
     public string targetTag; // The tag of the object that should trigger the scene load
+    public TMP_Text uiText; // Reference to the UI Text component
+    public GameObject portalPopUp;
 
-    // Method to handle trigger collision and load the specified scene
+    private bool isPlayerInTrigger = false; // Flag to check if player is in the trigger zone
+
+    // Method to handle entering the trigger zone
     private void OnTriggerEnter(Collider other)
     {
         // Check if the collided object has the target tag
         if (other.CompareTag(targetTag))
+        {
+            isPlayerInTrigger = true;
+            portalPopUp.SetActive(true);
+        }
+    }
+
+    // Method to handle exiting the trigger zone
+    private void OnTriggerExit(Collider other)
+    {
+        // Check if the collided object has the target tag
+        if (other.CompareTag(targetTag))
+        {
+            isPlayerInTrigger = false;
+            portalPopUp.SetActive(false);
+        }
+    }
+
+    private void Update()
+    {
+        // Check if the player is in the trigger zone and the "E" key is pressed
+        if (isPlayerInTrigger && Input.GetKeyDown(KeyCode.E))
         {
             // Check if the scene name is valid
             if (!string.IsNullOrEmpty(sceneNameToLoad) && Application.CanStreamedLevelBeLoaded(sceneNameToLoad))
